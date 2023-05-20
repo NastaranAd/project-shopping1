5f3cc1758adc70ceae7aef9e3fe70864b4ac9679
Nastaran <kosarab368@gmail.com>
2023-05-20 23:39:02 +0430

    projectphase2-4

--- src/main/java/Model/Product.java
+++ src/main/java/Model/Product.java
@@ -1,86 +1,113 @@
 package Model;
+
 import java.util.ArrayList;
 
 
-public class Product implements Comparable{
-    private int opinionNumber=0;
+public class Product implements Comparable {
+    private int opinionNumber = 0;
     private long productId;
     private int capacity;
     private double productPrice;
     private String productName;
     private double averageScore;
-     private String comment;
-    final private ArrayList<Opinion>opinions=new ArrayList<>();
+    private String comment;
+    final private ArrayList<Opinion> opinions = new ArrayList<>();
     public category product;
-    public ArrayList<Opinion>getOpinions()
-    {
+
+    public ArrayList<Opinion> getOpinions() {
         return opinions;
     }
-    public Product(String productName,double productPrice,double averageScore ,int capacity,category product,String comment)
-    {
-       this.productPrice=productPrice;
-       this.averageScore=averageScore;
-       this.capacity=capacity;
-       this.productName=productName;
-       this.product=product;
-       this.comment=comment;
+
+    public Product(String productName, double productPrice, double averageScore, int capacity, category product, String comment) {
+        this.productPrice = productPrice;
+        this.averageScore = averageScore;
+        this.capacity = capacity;
+        this.productName = productName;
+        this.product = product;
+        this.comment = comment;
     }
-    public String  getProductName()
-    {
+
+    public String getProductName() {
         return this.productName;
     }
-    public double getProductPrice()
-    {
+
+    public double getProductPrice() {
         return this.productPrice;
     }
-    public category getCategory()
-    {
+
+    public category getCategory() {
         return this.product;
     }
-    public int getCapacity()
-    {
+
+    public int getCapacity() {
         return this.capacity;
     }
 
     @Override
     public int compareTo(Object o) {
         Product product100 = (Product) o;
-       if (this.productName.compareTo(product100.productName)<0)
-       {
-           return -1;}
-       else if (this.productName.compareTo(product100.productName)==0)
-           return 0;
-       else if (this.productName.compareTo(product100.productName)>0)
-           return 1;
+        if (this.productName.compareTo(product100.productName) < 0) {
+            return -1;
+
+        } else if (this.productName.compareTo(product100.productName) == 0) {
+            if (this.averageScore < product100.averageScore) {
+                return -1;
+            } else if (this.averageScore > product100.averageScore) {
+                return 1;
+            } else if (this.averageScore == product100.averageScore) {
+                if (this.productPrice<product100.productPrice)
+                {
+                    return -1;
+                }
+                else if(this.productPrice>product100.productPrice)
+                {
+                    return 1;
+                }
+                else if (this.productPrice==product100.productPrice)
+                {
+                    if (this.capacity<product100.capacity)
+                    {
+                        return -1;
+                    }
+                    else if (this.capacity>product100.capacity)
+                    {
+                        return 1;
+                    }
+                }
+            }
+        } else if (this.productName.compareTo(product100.productName) > 0) {
+            return 1;
+        }
+        return 0;
     }
 
     public enum category {
         DIGITAL, STATIONARY, VEHICLES, FOOD
     }
-    public String  setProductName(String name)
-    {
-        return this.productName=name;
+
+    public String setProductName(String name) {
+        return this.productName = name;
     }
-    public int  setProductCapacity(int capacity)
-    {
-        return this.capacity=capacity;
+
+    public int setProductCapacity(int capacity) {
+        return this.capacity = capacity;
     }
-    public double  setProductprice(double price)
-    {
-        return this.productPrice=price;
+
+    public double setProductprice(double price) {
+        return this.productPrice = price;
     }
-    public int  setProductCapacitymines()
-    {
-        return this.capacity= this.capacity-1;
+
+    public int setProductCapacitymines() {
+        return this.capacity = this.capacity - 1;
     }
-    public String  setProductcomment(String comment1)
-    {
-        return this.comment=comment1;
+
+    public String setProductcomment(String comment1) {
+        return this.comment = comment1;
     }
-    public double  setProductScore(double sum)
-    {
-         this.averageScore=averageScore+sum;
-        return this.averageScore/=opinionNumber;
+
+    public double setProductScore(double sum) {
+        this.averageScore = averageScore + sum;
+        return this.averageScore /= opinionNumber;
     }
 
     public int getOpinionNumber() {
@@ -92,11 +119,10 @@ public class Product implements Comparable{
     }
 
     @Override
-    public String toString()
-    {
-        return "product name : "+ productName+", product price : "+productPrice+", product category : "+product+", product capacity : "+capacity+",product average score : "+averageScore+" , comment : "+comment;
+    public String toString() {
+        return "product name : " + productName + ", product price : " + productPrice + ", product category : " + product + ", product capacity : " + capacity + ",product average score : " + averageScore + " , comment : " + comment;
     }
-    }
+}
 
 
 

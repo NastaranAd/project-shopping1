5b3f5d1f76c63ebd045c363f5ff353c377693480
Nastaran <kosarab368@gmail.com>
2023-05-28 00:55:07 +0330

    projectphase2-21

--- src/main/java/com/example/demoprojectphase22/Country.java
+++ src/main/java/com/example/demoprojectphase22/Country.java
@@ -1,9 +1,17 @@
 package com.example.demoprojectphase22;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class Country {
     @FXML
@@ -16,8 +24,12 @@ public class Country {
     private Button backButton;
 
     @FXML
-    void backButton(MouseEvent event) {
-
+    void backButton(MouseEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("filter.fxml")));
+        Stage stage = (Stage) ((Node)(event.getSource())).getScene().getWindow();
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
